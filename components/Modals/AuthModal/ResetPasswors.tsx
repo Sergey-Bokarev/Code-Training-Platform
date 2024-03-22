947b783356193af0dc1731280a1181920652a2b3
Sergey Bokarev <bokarev_87@mail.ru>
2024-03-22 20:25:01 +0300

    reset password functionality

--- components/Modals/AuthModal/ResetPasswors.tsx
+++ components/Modals/AuthModal/ResetPasswors.tsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { auth } from "@/firebase/firebase";
+import { useSendPasswordResetEmail } from "react-firebase-hooks/auth";
 
 type ResetPasswordProps = {};
 
 const ResetPassword: React.FC<ResetPasswordProps> = () => {
 
-    return <form className="space-y-6 px-6 lg:px-8 pb-4 sm:pb-6 xl:pb-8">
+    const [email, setEmail] = useState<string>("");
+    const [sendPasswordResetEmail, sending, error] = useSendPasswordResetEmail(auth);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value);
+    };
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (sending) return;
+        const success = await sendPasswordResetEmail(email);
+        if (success) {
+            alert("Send email");
+        }
+    };
+
+    useEffect(() => {
+        if (error) {
+            alert(error.message);
+        }
+    }, [error]);
+
+    return <form className="space-y-6 px-6 lg:px-8 pb-4 sm:pb-6 xl:pb-8" onSubmit={handleSubmit}>
         <h3 className="text-xl font-medium text-white">Reset Password</h3>
         <p className="text-sm text-white">
             Forgotten your password? Enter your e-mail address below, and we&apos;ll send you an e-mail allowing you to reset it.
@@ -17,6 +41,7 @@ const ResetPassword: React.FC<ResetPasswordProps> = () => {
                 type="email"
                 name="email"
                 id="email"
+                onChange={handleChange}
                 className="
                     border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5
                     bg-gray-600 border-gray-500 placeholder-gray-400 text-white
