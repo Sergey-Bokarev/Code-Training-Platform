bfda46ee051a159510ca9930195ae3b4cc42b8f7
Sergey Bokarev <bokarev_87@mail.ru>
2024-04-08 10:46:10 +0300

    add problems JSONs

--- components/Workspace/ProblemDescription/ProblemDescription.tsx
+++ components/Workspace/ProblemDescription/ProblemDescription.tsx
@@ -42,15 +42,7 @@ const ProblemDescription: React.FC<ProblemDescriptionType> = () => {
                             </div>
                         </div>
                         <div className='text-white text-sm'>
-							<p className='mt-3'>
-								Given an array of integers <code>nums</code> and an integer <code>target</code>, return
-								<em>indices of the two numbers such that they add up to</em> <code>target</code>.
-							</p>
-							<p className='mt-3'>
-								You may assume that each input would have <strong>exactly one solution</strong>, and you
-								may not use thesame element twice.
-							</p>
-							<p className='mt-3'>You can return the answer in any order.</p>
+							
 						</div>
                         <div className='mt-4'>
 							<div>
@@ -89,19 +81,7 @@ const ProblemDescription: React.FC<ProblemDescriptionType> = () => {
                         <div className='my-5'>
 							<div className='text-white text-sm font-medium'>Constraints:</div>
 							<ul className='text-white ml-5 list-disc'>
-								<li className='mt-2'>
-									<code>2 ≤ nums.length ≤ 10</code>
-								</li>
-
-								<li className='mt-2'>
-									<code>-10 ≤ nums[i] ≤ 10</code>
-								</li>
-								<li className='mt-2'>
-									<code>-10 ≤ target ≤ 10</code>
-								</li>
-								<li className='mt-2 text-sm'>
-									<strong>Only one valid answer exists.</strong>
-								</li>
+								
 							</ul>
 						</div>
                     </div>
