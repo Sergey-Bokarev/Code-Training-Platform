c257c72b34e76edb3c056fdf654cd754f2cd1602
Sergey Bokarev <bokarev_87@mail.ru>
2024-04-08 12:16:31 +0300

    testcases tabs logic

--- components/Workspace/Playground/TestCases/TestCases.tsx
+++ components/Workspace/Playground/TestCases/TestCases.tsx
@@ -1,11 +1,13 @@
+import React, { useState } from "react";
 import { Example, Problem } from "@/utils/types/problem";
-import React from "react";
 
 type TestCasesProps = {
     problem: Problem;
 };
 
 const TestCases: React.FC<TestCasesProps> = ({problem}) => {
+
+    const [activeTestCaseId, setActiveTestCaseId] = useState<number>(0);
 
     return (
         <div className="w-full px-5 overflow-auto">
@@ -17,9 +19,9 @@ const TestCases: React.FC<TestCasesProps> = ({problem}) => {
             </div>
             <div className="flex">
                 {problem.examples.map((example: Example, idx: number) => (
-                    <div key={example.id} className="mr-2 items-start mt-2 text-white">
+                    <div key={example.id} className="mr-2 items-start mt-2" onClick={() => setActiveTestCaseId(example.id)}>
                         <div className="flex flex-wrap items-center gap-y-4">
-                            <div className="tabBtn">
+                            <div className={`tabBtn ${activeTestCaseId === idx ? "text-white" : "text-gray-400"}`}>
                                 {`Case ${idx + 1}`}
                             </div>
                         </div>
@@ -29,11 +31,11 @@ const TestCases: React.FC<TestCasesProps> = ({problem}) => {
             <div className="font-semibold my-4">
                 <p className="text-sm font-medium mt-4 text-white">Input:</p>
                 <div className="w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2">
-                    {problem.examples[0].inputText}
+                    {problem.examples[activeTestCaseId].inputText}
                 </div>
                 <p className="text-sm font-medium mt-4 text-white">Output:</p>
                 <div className="w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2">
-                    {problem.examples[0].outputText}
+                    {problem.examples[activeTestCaseId].outputText}
                 </div>
             </div>
         </div>
