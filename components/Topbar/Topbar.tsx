233406dd9f864b88930ee1249a6b73ae9272190e
Sergey Bokarev <bokarev_87@mail.ru>
2024-04-22 20:01:51 +0300

    problem page hide timer on logout

--- components/Topbar/Topbar.tsx
+++ components/Topbar/Topbar.tsx
@@ -56,7 +56,7 @@ const Topbar:React.FC<TopbarProps> = ({
                     </div>
                 )}
                 <div className="flex items-center space-x-4 flex-1 justify-end">
-                    {problemPage && <Timer />}
+                    {user && problemPage && <Timer />}
                     {!user
                         ?   <Link href="/auth" onClick={handleClick}>
                                 <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded" >
