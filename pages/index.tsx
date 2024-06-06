a0d11612474bdf8e27acd4299be0a231c607347d
Sergey Bokarev <bokarev_87@mail.ru>
2024-06-06 12:45:26 +0300

    fix hydration error

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,13 @@ import { useState } from "react";
 import ProblemsTable from "@/components/ProblemsTable/ProblemsTable";
 import Topbar from "@/components/Topbar/Topbar";
 import TableRowSkeleton from "@/components/Skeletons/TableRowSkeleton";
+import useHasMounted from "@/hooks/useHasMounted";
 
 export default function Home() {
   const [loadingProblems, setLoadingProblems] = useState<boolean>(true);
+  const hasMounted = useHasMounted();
+
+  if (!hasMounted) return null;
   
   return (
     <main className="bg-dark-layer-2 min-h-screen">
